21a53ff831a00674dbd574a7df05c925677e24f6
Manobalaarul <manobalaarul3@gmail.com>
2024-07-24 12:55:33 +0530

    route structure

--- main.js
+++ main.js
@@ -7,27 +7,7 @@ app.get('/',(req,res) =>{
     res.json({msg: "Hello students!"});
 })
 
-// CRUD Functionality of Movies
 
-// R - For Reading
-app.get('/movies',()=>{
-
-});
-
-// C - For Creating Movies
-app.post('/movies',()=>{
-
-});
-
-// U - For Updating movies
-app.put('/movies/:id',()=>{
-
-});
-
-// D - For Deleting movies
-app.delete('/movies/:id',()=>{
-
-});
 
 app.listen(PORT,() =>{
     console.log(`The server is running at http://localhost:${PORT}`);
---
+++ routes/movies.route.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+// CRUD Functionality of Movies
+
+// R - For Reading
+router.get('/movies',()=>{
+
+});
+
+// C - For Creating Movies
+router.post('/movies',()=>{
+
+});
+
+// U - For Updating movies
+router.put('/movies/:id',()=>{
+
+});
+
+// D - For Deleting movies
+router.delete('/movies/:id',()=>{
+
+});
