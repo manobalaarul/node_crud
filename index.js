81c40de7382e81b3c20e6b7249fb015bca67f8a3
Manobalaarul <manobalaarul3@gmail.com>
2024-08-07 14:53:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import router from './routes/movies.route.js';
 import connectDB from './lib/db.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const PORT = 6969;
+const PORT = process.env.PORT || 5000;
 
 // Data understanting 
 app.use(express.json());
